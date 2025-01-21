d23ec29b5debd669a90ad7343243b9489b258336
Ilya Gulya <ilya@gulya.me>
2025-01-21 10:52:13 +0500

    Nits

--- sample-web/src/commonMain/kotlin/com/example/sqldelight/hockey/db/DateFormatHelper.kt
+++
@@ -1,3 +0,0 @@
-package com.example.sqldelight.hockey.db
-
-expect fun Date.formatted(format: String): String
--- sample-web/src/wasmJsMain/kotlin/com/example/sqldelight/hockey/db/DateFormatHelper.kt
+++ sample-web/src/wasmJsMain/kotlin/com/example/sqldelight/hockey/db/DateFormatHelper.kt
@@ -2,5 +2,5 @@ package com.example.sqldelight.hockey.db
 
 import kotlinx.datetime.internal.JSJoda.DateTimeFormatter
 
-actual fun Date.formatted(format: String): String =
+fun Date.formatted(format: String): String =
   DateTimeFormatter.ofPattern(format).format(this)
